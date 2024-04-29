82da8e3389a5876ba8b753eef34862bc9e325cbf
LINUSOBURA <linusobura@gmail.com>
2024-04-29 23:06:22 +0300

    updated stats_get function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """App views module"""
+import models
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -14,14 +21,13 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats_get():
     classes = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users"
+        'states': State,
+        'users': User,
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review
     }
-    stats_dict = {}
-    for cls, route in classes.items():
-        stats_dict[route] = storage.count(cls)
-    return jsonify(stats_dict)
+    for cls in classes:
+        classes[cls] = storage.count(classes[cls])
+    return jsonify(classes)
