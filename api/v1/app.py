24271e9499bb5892268d217b6f6c1100833361f5
LINUSOBURA <linusobura@gmail.com>
2024-04-30 09:35:31 +0300

    updated stats_get function

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ v1 module for the api
 import os
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 
 app = Flask(__name__)
@@ -17,6 +17,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_views(exception):
     """Close method to close views"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
