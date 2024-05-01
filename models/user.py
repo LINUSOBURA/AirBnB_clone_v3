7807eaf659d414bb77bc4034585d928794c80751
LINUSOBURA <linusobura@gmail.com>
2024-05-01 17:40:09 +0300

    Hash password

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
-from models.base_model import BaseModel, Base
+import hashlib
 from os import getenv
+
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+import models
+from models.base_model import Base, BaseModel
 
 
 class User(BaseModel, Base):
@@ -26,4 +29,7 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if 'password' in kwargs:
+            kwargs['password'] = hashlib.md5(
+                kwargs['password'].encode()).hexdigest()
         super().__init__(*args, **kwargs)
