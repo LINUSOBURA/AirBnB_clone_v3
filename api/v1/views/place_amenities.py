655b863d8b9584185edc7c2e1df22f046e42b1f3
LINUSOBURA <linusobura@gmail.com>
2024-05-01 16:58:28 +0300

    place_amenities

---
+++ api/v1/views/place_amenities.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Place Amenities module"""
+from api.v1.views import app_views
+from flask import abort, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+
+
+@app_views.route('/places/<place_id>/amenities',
+                 methods=['GET'],
+                 strict_slashes=False)
+def get_place_amenities(place_id):
+    """Get all amenities of a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    if storage.__class__.__name__ == 'DBStorage':
+        return jsonify([amenity.to_dict() for amenity in place.amenities])
+
+    return jsonify([amenity.to_dict() for amenity in place.amenity_ids])
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """Delete an amenity from a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+
+    if storage.__class__.__name__ == 'DBStorage':
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+        storage.save()
+    else:
+        if amenity_id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity_id)
+        storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'],
+                 strict_slashes=False)
+def link_place_amenity(place_id, amenity_id):
+    """Link an amenity to a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+
+    if storage.__class__.__name__ == 'DBStorage':
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
+        place.amenities.append(amenity)
+        storage.save()
+    else:
+        if amenity_id in place.amenity_ids:
+            return jsonify(amenity.to_dict()), 200
+        place.amenity_ids.append(amenity_id)
+        storage.save()
+
+    return jsonify(amenity.to_dict()), 201
